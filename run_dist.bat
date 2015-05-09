c9a3e19edb3c5b86b1a543837abdadb4b2466f2d
CaptainSpleXx <captainsplexx@googlemail.com>
2015-05-09 12:31:24 +0200

    -

--- run_dist.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title FrostBite3Editor Console
-mode con: cols=130 lines=20> nul
-echo DO NOT CLOSE THIS WINDOW, ITS REQUIRED!
-echo JavaFX does only works on Java 8+
-java -jar dist.jar
-pause
